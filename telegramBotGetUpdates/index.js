80531865b2b23db09cd349f01f0d24d84a499fb2
AizenSousuke <puteranikdimas@yahoo.com.sg>
2023-07-17 19:06:35 +0800

    Added openai

--- telegramBotGetUpdates/index.js
+++ telegramBotGetUpdates/index.js
@@ -11,22 +11,31 @@ const BOT = new Telegraf(process.env.TELEGRAM_BOT_TOKEN, {
 });
 
 BOT.telegram.setWebhook(process.env.WEBHOOK_URL);
-BOT.start((ctx) => ctx.reply("Welcome to the Misaka Network."));
+// BOT.start((ctx) => ctx.reply("Welcome to the Misaka Network."));
 // Echo
 // BOT.on("message", (ctx) => ctx.telegram.sendMessage(ctx.chat.id, ctx.message));
 
-// BOT.on("message", async (ctx) => {
-// 	const chatId = ctx.chat.id;
-// 	// const prompt = RemoveCommand(ctx.message.text);
-// 	const response = await openai
-// 		.OPEN_AI()
-// 		.createCompletion(completion.CompletionModel({ prompt: ctx.message.text }));
+BOT.on("message", async (ctx) => {
+	const chatId = ctx.chat.id;
+	// const prompt = RemoveCommand(ctx.message.text);
+	const response = await openai
+		.OPEN_AI()
+		.createCompletion(
+			completion.CompletionModel({ prompt: ctx.message.text })
+		);
 
-// 	if (response) {
-// 		await ctx.telegram.sendMessage(chatId, response.data.choices[0].text);
-// 	}
-// });
+	if (response) {
+		await ctx.telegram.sendMessage(chatId, response.data.choices[0].text);
+	}
+});
 
 module.exports = async function (context, req) {
-	return await BOT.handleUpdate(req.body, context.res);
+	try {
+		return await BOT.handleUpdate(req.body, context.res);
+	} catch (error) {
+		return {
+			statusCode: 500,
+			body: error
+		}
+	}
 };
